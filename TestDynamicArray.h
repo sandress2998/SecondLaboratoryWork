e00fa3cdb81d96dc8d610a5891df7ea1c5d2c8be
sandress2998 <149189942+sandress2998@users.noreply.github.com>
2024-05-22 01:20:02 +0300

    Update TestDynamicArray.h

--- TestDynamicArray.h
+++ TestDynamicArray.h
@@ -9,5 +9,8 @@ public:
     void testGetSize();
     void testSet();
     void testResize();
+    void testAppend();
+    void testPrepend();
+    void testInsertAt();
+    void testAssignmentOperator();
 };
-
