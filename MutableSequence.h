1146d1d104712d74b40da3d0e2bb9547e399002a
sandress2998 <149189942+sandress2998@users.noreply.github.com>
2024-05-22 01:15:11 +0300

    Update MutableSequence.h

--- MutableSequence.h
+++ MutableSequence.h
@@ -7,13 +7,12 @@ class MutableSequence: public Sequence<T> {
 public:
     virtual ~MutableSequence() = default;
 
-    virtual void set(int index, const T& value) = 0; // в ImmutableSequence функция отсутствует
-
     virtual void append(const T& item) = 0;
     virtual void prepend(const T& item) = 0;
     virtual void insertAt(int index, const T& item) = 0;
 
-    virtual T& operator[](int index) const = 0; // элемент возвращается по ссылке, т.е. его можно изменять
+    virtual T& operator[](int index) = 0;
+    virtual void set(int index, const T& value) = 0; // в ImmutableSequence функция отсутствует
 };
 
 
