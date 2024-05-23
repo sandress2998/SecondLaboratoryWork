0adc1c2f0f287753af7989edfca5fceb1a15db78
sandress2998 <149189942+sandress2998@users.noreply.github.com>
2024-05-23 12:15:57 +0300

    Update ImmutableSequence.h

--- ImmutableSequence.h
+++ ImmutableSequence.h
@@ -10,4 +10,6 @@ public:
     virtual ImmutableSequence<T>* append(const T& item) const = 0;
     virtual ImmutableSequence<T>* prepend(const T& item) const = 0;
     virtual ImmutableSequence<T>* insertAt(int index, const T& item) const = 0;
+
+    virtual ImmutableSequence<T>* set(int index, const T& item) const = 0;
 };
